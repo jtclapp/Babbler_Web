accb33107bdc64b8df9a109235d7b37618dc167b
jtclapp <67022934+jtclapp@users.noreply.github.com>
2021-08-24 11:07:41 -0500

    Add files via upload
    Spring Boot Structure Created

---
+++ platform/platform/src/test/java/com/web/CSChat/platform/ConspiracyTheoryChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.CSChat.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConspiracyTheoryChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
