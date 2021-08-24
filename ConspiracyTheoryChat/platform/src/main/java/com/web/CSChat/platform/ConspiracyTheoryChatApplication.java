48e2f5049ab1745f25a9372ac4ce16f30c01293f
jtclapp <67022934+jtclapp@users.noreply.github.com>
2021-08-24 11:12:39 -0500

    Add files via upload
    Spring Boot Structure Created

---
+++ ConspiracyTheoryChat/platform/src/main/java/com/web/CSChat/platform/ConspiracyTheoryChatApplication.java
@@ -0,0 +1,12 @@
+package com.web.CSChat.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConspiracyTheoryChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConspiracyTheoryChatApplication.class, args);
+	}
+}
