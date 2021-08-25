ba0d8e8cb6291a941fb52b0b368ff5897828a005
Jared Clapp <jtclapp10@gmail.com>
2021-08-24 22:09:18 -0500

    Connected Firebase To Spring Boot

---
+++ ConspiracyTheoryChat/platform/src/main/java/com/web/CSChat/platform/service/FBInitializer.java
@@ -0,0 +1,31 @@
+package com.web.CSChat.platform.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class FBInitializer {
+    FirebaseApp firebaseApp;
+
+    public FBInitializer() throws IOException {
+        InputStream file = getClass().getResourceAsStream("/key.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(file))
+                .setDatabaseUrl("https://conspiracy-theory-chat-default-rtdb.firebaseio.com")
+                .build();
+
+        firebaseApp = null;
+        List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+        if(firebaseApps != null && !firebaseApps.isEmpty())
+        {
+
+        }
+        else {
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+    }
+}
