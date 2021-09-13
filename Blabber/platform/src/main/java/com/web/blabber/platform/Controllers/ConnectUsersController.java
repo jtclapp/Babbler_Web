6c348ce065a8c2599ea5d08e06b624f211a863c0
Jared Clapp <jtclapp10@gmail.com>
2021-09-13 11:22:19 -0500

    Changed Base Name

---
+++ Blabber/platform/src/main/java/com/web/blabber/platform/Controllers/ConnectUsersController.java
@@ -0,0 +1,22 @@
+package com.web.blabber.platform.Controllers;
+
+import com.web.blabber.platform.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.concurrent.ExecutionException;
+
+@Controller
+public class ConnectUsersController
+{
+    UserService userService;
+
+    @GetMapping("/connect")
+    public String loadConnectUsersPage(Model model) throws ExecutionException, InterruptedException {
+        System.out.println("Switching over to the connect users page.");
+        userService = new UserService();
+        model.addAttribute("allUsers",userService.getAllUsers());
+        return "connectUsers";
+    }
+}
