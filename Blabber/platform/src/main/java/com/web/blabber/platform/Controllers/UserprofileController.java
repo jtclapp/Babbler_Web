4491e06d8e14edd355f3fb6bc925d4c612edfe29
KeenanConger <89274857+KeenanConger@users.noreply.github.com>
2021-10-27 13:39:19 -0500

    added link to user profile page on dropdown

--- Blabber/platform/src/main/java/com/web/blabber/platform/Controllers/UserprofileController.java
+++ Blabber/platform/src/main/java/com/web/blabber/platform/Controllers/UserprofileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserprofileController
 {
-    @GetMapping("/Userprofile")
+    @GetMapping("/userprofile")
     public String loadUserprofilePage(Model model)
     {
         Authentication user_authentication = SecurityContextHolder.getContext().getAuthentication();
